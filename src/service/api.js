f2460011963c495775f3d2acee3ebcf9a992f184
guilhermemd <guilherme.md@gmail.com>
2021-06-07 15:27:37 -0300

    first

---
+++ src/service/api.js
@@ -0,0 +1,12 @@
+import { getImage, failedRequest, requestDog } from '../redux/actions/actions';
+export function fetchDog() {
+  return (dispatch) => {
+    dispatch(requestDog());
+    return fetch('https://dog.ceo/api/breeds/image/random')
+      .then((r) => r.json()
+        .then(
+          (json) => dispatch(getImage(json)),
+          (error) => dispatch(failedRequest(error)),
+        ));
+  };
+}
